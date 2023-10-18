61a5be22351472fba72d220170c86151fa66f67d
Nathan Lam <70554687+nathanklam@users.noreply.github.com>
2023-10-18 09:59:26 -0700

    10/18 update
    reupload due to moving things around (in case)

---
+++ rhythm - Copy (2)/scripts/shop.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_to_lobby_pressed():
+	SoundFx.button_click()
+	SceneTransition.change_scene_to_file("res://scenes/lobby.tscn")
+	$input_stop.show()
+	
+func _on_to_secret_pressed():
+	SoundFx.button_click()
+	SceneTransition.change_scene_to_file("res://scenes/secret_battle.tscn")
+	$input_stop.show()
+	await get_tree().create_timer(0.5).timeout
+	BgMusic.stop()
